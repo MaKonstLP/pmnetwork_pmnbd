64dc118f8e1e5de84e3d1ccf2f439791adfbb7fc
Martynov Konstantin <martynov@liderpoiska.ru>
2022-11-23 14:47:34 +0100

    Доработал попап карту

--- web/src/js/components/map.js
+++ web/src/js/components/map.js
@@ -75,4 +75,62 @@ export default class YaMap {
     }
 
 
+    mapPopup() {
+
+        ymaps.ready(function () {
+            console.log('mapPopup');
+            var parent = $('.map.map-popup');
+            var myMap = new ymaps.Map('map-popup', {
+                    center: [
+                        parent.find('#map-popup').data('mapdotx'),
+                        parent.find('#map-popup').data('mapdoty'),
+                    ],
+                    zoom: 15,
+                    behaviors: ["drag", "dblClickZoom", "rightMouseButtonMagnifier", "multiTouch"]
+                }),
+
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                    hintContent: 'hintContent',
+                    balloonContent: '\
+                            <div class="balloon_content_wrap">\
+                                <div class="balloon_content_img">\
+                                    <img src="' + parent.find('.map_img').attr('src') + '">\
+                                </div>\
+                                <div class="ballon_content_text">\
+                                    <a class="ballon_content_name" href="' + parent.find('.map_rest_name').attr('href') + '">' + parent.find('.map_rest_name').html() + '</a>\
+                                    <p class="ballon_content_address">' + parent.find('.map_rest_address').html() + '</p>\
+                                </div>\
+                            </div>\
+                    ',
+                }, {
+                    //iconLayout: 'default#image',
+                    preset: 'islands#darkGreenIcon',
+                    hideIconOnBalloonOpen: false,
+                    balloonOffset: [0, -37],
+                });
+
+
+            if (window.matchMedia('(max-width: 768px)').matches) {
+                myMap.behaviors.disable('scrollZoom');
+                myMap.behaviors.disable('drag');
+                myMap.events.add('click', () => {
+                    myMap.behaviors.enable('scrollZoom');
+                    myMap.behaviors.enable('drag');
+                });
+                const zoom = myMap.controls.get('zoomControl');
+                zoom.events.add('click', () => {
+                    myMap.behaviors.enable('scrollZoom');
+                    myMap.behaviors.enable('drag');
+                });
+            }
+
+            myMap.geoObjects.add(myPlacemark);
+
+            if ($(window).width() > 600) {
+                myPlacemark.balloon.open();
+            }
+        });
+
+    }
+
 }
